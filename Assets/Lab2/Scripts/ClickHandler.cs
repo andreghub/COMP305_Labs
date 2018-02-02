e82490ca8621bc0d7dd21f4abf9333391c3a6eb7
Andre Oliveira <andreoliveira.it@gmail.com>
2018-02-02 14:41:40 -0500

    Lab 2

---
+++ Assets/Lab2/Scripts/ClickHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour
+{
+
+    public GameObject AttachedObject;
+    public Sprite Sprite;
+
+    private SpriteRenderer sRend;
+
+    // Use this for initialization
+    void Start()
+    {
+        sRend = AttachedObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        sRend.sprite = Sprite;
+    }
+}
---
+++ Assets/Lab2/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(2, 0);
+	}
+
+
+}
